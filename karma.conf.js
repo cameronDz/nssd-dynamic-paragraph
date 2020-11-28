a2c32876346561c3cd53ab14894d17b50321c56d
cameronDz <camerondziurgot@gmail.com>
2020-11-28 02:24:36 -0500

    changes to testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+// const jasmineReporters = require('jasmine-reporters');
 
 module.exports = function (config) {
   config.set({
@@ -27,14 +28,38 @@ module.exports = function (config) {
     frameworks: ['@angular-devkit/build-angular', 'jasmine'],
     logLevel: config.LOG_INFO,
     plugins: [
+      // jasmineReporters,
       require('karma-chrome-launcher'),
       require('karma-coverage'),
       require('karma-jasmine'),
+      // require('@types/jasmine'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     port: 9876,
-    reporters: ['coverage', 'coverage-istanbul', 'progress'],
+    reporters: [
+      'coverage',
+      'coverage-istanbul',
+      'progress',
+      'junit'
+      // new jasmineReporters.JUnitXmlReporter({
+      //   consolidateAll: true,
+      //   savePath: './',
+      //   filePrefix: 'xmlresults' })
+      ],
+
+    // the default configuration
+    junitReporter: {
+      outputDir: require('path').join(__dirname, './coverage'), // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'test', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      suite: '', // suite will become the package name attribute in xml testsuite element
+      useBrowserName: true, // add browser name to report and classes names
+      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+      properties: {}, // key value pair of properties to add to the <properties> section of the report
+      xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+    },
     restartOnFileChange: false,
     singleRun: true
   });
